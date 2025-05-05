0bfc3b2aef89b9ad7e4831011c7857296080570b
DavideCotigliani <davide.cotigliani@gmail.com>
2025-05-05 15:52:41 +0200

    [UPD] First part of the main completed, background-color black

---
+++ react-dc-comics/src/components/Main.jsx
@@ -0,0 +1,12 @@
+
+const Main = () => {
+    return (
+        <div className="blackContainer">
+            <div className="firstLine">
+                <h1>--&gt;Content goes here&lt;--</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Main
