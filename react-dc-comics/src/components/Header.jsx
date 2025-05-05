26867d7556eda5d74136f000967230dd2ee33bbc
DavideCotigliani <davide.cotigliani@gmail.com>
2025-05-05 15:32:52 +0200

    [UPD] Header section completed, just need to adjust the hover part

--- react-dc-comics/src/components/Header.jsx
+++ react-dc-comics/src/components/Header.jsx
@@ -1,8 +1,29 @@
+import immagine from '../assets/img/dc-logo.png'
+
 const Header = () => {
     return (
-        <div>
-            <h2>Ciao</h2>
+        <div className="container">
+            <header>
+                <figure>
+                    <img src={immagine} alt="" />
+                </figure>
+                <nav>
+                    <ul>
+                        <li><a href="#">Characters</a></li>
+                        <li><a href="#" className='active'>Comics</a></li>
+                        <li><a href="#">Movies</a></li>
+                        <li><a href="#">TV</a></li>
+                        <li><a href="#">Games</a></li>
+                        <li><a href="#">Collectibles</a></li>
+                        <li><a href="#">Videos</a></li>
+                        <li><a href="#">Fans</a></li>
+                        <li><a href="#">News</a></li>
+                        <li><a href="#">Shop</a></li>
+                    </ul>
+                </nav>
+            </header>
         </div>
+
     )
 }
 
